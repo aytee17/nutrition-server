2bedc2deb9f504db39700f9009eadf8226e3a5db
Andrew Tang <andrew.tang@outlook.com>
2018-07-27 14:15:22 +1000

    Add script to populate ingredients table

---
+++ sql/populate.sql
@@ -0,0 +1,15 @@
+INSERT INTO ingredients (
+	name, 
+	units, 
+	energy, 
+	protein, 
+	fat, 
+	saturated,
+	carbohydrates,
+	sugars,
+	sodium) VALUES
+('Spaghetti', 		'g', 		1530, 	12, 	2, 		0, 		72, 	0,0),
+('Olive Oil', 		'mL', 		3390, 	0, 		91, 	0, 		0,		0,0),
+('Pancetta', 		'g', 		778, 	18.1, 	13.2, 	0, 		1,		0,0),
+('Egg', 			'amount', 	290.5, 	6.35, 	5.15, 	0, 		0.7,	0,0),
+('Parmesan Cheese', 'g', 		165, 	33, 	28, 	0, 		0,		0,0);
